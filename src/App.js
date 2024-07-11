4e38e76b60f41938072f184c30e0e7d70e0b4b8a
jamesperez768 <jpyv9@umsystem.edu>
2024-07-11 14:16:56 -0500

    Add Three.js page and update navigation menu

--- src/App.js
+++ src/App.js
@@ -5,19 +5,35 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import EventCalendar from './pages/EventCalendar';
 import Menu from './pages/Menu';
+import Three from './pages/Three.js';
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/calendar">Event Calendar</Link>
-        <Link to="/menu">Menu</Link>
-      </nav>
+      <div className="flex bg-gray-800" style={{ backgroundColor: '#4E598C' }}>
+        <img src={require('./sipnplay.png')} alt="Logo" className="w-20 h-20" />
+        <nav className="p-4 ml-auto">
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/" className="text-white hover:text-gray-400">Home</Link>
+            </li>
+            <li>
+              <Link to="/calendar" className="text-white hover:text-gray-400">Event Calendar</Link>
+            </li>
+            <li>
+              <Link to="/menu" className="text-white hover:text-gray-400">Menu</Link>
+            </li>
+            <li>
+              <Link to="/three" className="text-white hover:text-gray-400">Three</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/calendar" element={<EventCalendar />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path="/three" element={<Three />} />
       </Routes>
     </Router>
   );
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Box } from '@react-three/drei';
+import { Box,Text} from '@react-three/drei';
 
 function Menu() {
+  const menuItems = [
+    { name: 'Burger', cost: '$10' },
+    { name: 'Pizza', cost: '$12' },
+    { name: 'Salad', cost: '$8' },
+    { name: 'Pasta', cost: '$9' },
+    // Add more items here
+  ];
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold">Menu</h1>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]}>
-          <meshStandardMaterial attach="material" color="orange" />
-        </Box>
-        <Box position={[1.2, 0, 0]}>
-          <meshStandardMaterial attach="material" color="blue" />
-        </Box>
-      </Canvas>
+      <div className="flex justify-center mt-8">
+        <div className="w-1/2">
+          <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            {menuItems.map((item, index) => (
+              <Box key={index} position={[-1.2 + index * 1.2, 0, 0]}>
+                <meshStandardMaterial attach="material" color="orange" />
+                <Text
+                  position={[0, 0, 0.5]}
+                  fontSize={0.2}
+                  anchorX="center"
+                  anchorY="middle"
+                  outlineColor="black"
+                  outlineWidth={0.02}
+                  outlineOpacity={1}
+                  color="white"
+                  textAlign="center"
+                >
+                  {`${item.name}\n${item.cost}`}
+                </Text>
+              </Box>
+            ))}
+          </Canvas>
+        </div>
+      </div>
     </div>
   );
 }
